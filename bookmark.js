464b57b3d74fa5d9db53f2b3531c175b06ae8de1
SSumu <85830782+SSumu@users.noreply.github.com>
2025-05-09 12:14:11 +0530

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,1 @@
+javascript:(function(){  let s=document.createElement('script');  s.src='https://ssumu.github.io/huawei-routers-webui-editor/huawei-browser-tool.js';  document.body.appendChild(s);})();
